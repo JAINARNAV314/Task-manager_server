e10f35122c1aac0e4d0310ea59d660d1982bb0ce
Arnav <jainarnav314@gmail.com>
2023-08-03 20:30:37 +0530

    updated

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 const dotenv=require('dotenv')
 dotenv.config()
-// const mongoURI="mongodb://localhost:27017/manager?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+// const mongoURI="mongodb://127.0.0.1:27017/manager?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
 const mongoURI=`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.agn0jto.mongodb.net/projectrepo?retryWrites=true&w=majority`
 const connectToMongo=()=>{
     mongoose.connect(mongoURI,()=>{
